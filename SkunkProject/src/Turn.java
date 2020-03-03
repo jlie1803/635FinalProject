f2a7dee31ac958c38261aded80e548e800c0f475
Esten Rye <esten.rye@ryezone.com>
2020-03-03 06:16:54 +0000

    Add initial Turn class. Adds initial failing test. Stubs out Turn class. Adds capability to provide an array to MockDie.

---
+++ SkunkProject/src/Turn.java
@@ -0,0 +1,16 @@
+
+public class Turn {
+
+	public Turn(Dice dice) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getScore() {
+		return -1;
+	}
+
+	public void roll() {
+		// TODO Auto-generated method stub
+		
+	}
+}
