e230266d91a1aa41a194f27f02f3765237b0d0a2
Esten Rye <esten.rye@ryezone.com>
2020-03-05 20:03:41 -0600

    Add failing test asserting that all rolls after a one is rolled must not change the turn score.

--- SkunkProject/test/TestTurn.java
+++ SkunkProject/test/TestTurn.java
@@ -19,6 +19,22 @@ class TestTurn {
 	}
 	
 	@Test
+	void when_a_one_is_rolled_subsequent_rolls_do_not_affect_the_score() {
+		Die mockDie1 = new MockDie(new int[] {5, 3, 1, 5});
+		Die mockDie2 = new MockDie(new int[] {4, 2, 9, 6});
+		assertEquals(5, mockDie1.getLastRoll());
+		assertEquals(4, mockDie2.getLastRoll());
+		Turn turn = new Turn(new Dice(mockDie1, mockDie2));
+		assertEquals(9, turn.getScore());
+		turn.roll();
+		assertEquals(14, turn.getScore());
+		turn.roll();
+		assertEquals(0, turn.getScore());
+		turn.roll();
+		assertEquals(0, turn.getScore());
+	}
+	
+	@Test
 	void turn_returns_a_non_zero_score_when_a_one_is_not_rolled() {
 		Die mockDie1 = new MockDie(new int[] {5, 3, 7});
 		Die mockDie2 = new MockDie(new int[] {4, 2, 9});
