6a9708594502bf9897801f63804ec1bb822d8450
Esten Rye <esten.rye@ryezone.com>
2020-03-05 19:23:56 -0600

    Add failing test for non-zero score.

--- SkunkProject/test/TestTurn.java
+++ SkunkProject/test/TestTurn.java
@@ -14,4 +14,15 @@ class TestTurn {
 		turn.roll();
 		assertEquals(0, turn.getScore());
 	}
+	
+	@Test
+	void turn_returns_a_non_zero_score_when_a_one_is_not_rolled() {
+		Die mockDie1 = new MockDie(new int[] {5, 3, 7});
+		Die mockDie2 = new MockDie(new int[] {4, 2, 9});
+		Turn turn = new Turn(new Dice(mockDie1, mockDie2));
+		turn.roll();
+		turn.roll();
+		turn.roll();
+		assertEquals(30, turn.getScore());
+	}
 }
